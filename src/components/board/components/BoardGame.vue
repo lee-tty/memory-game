479ed9a3db2ce0b03b7d8a286ef044391ade0bff
Letty Villamizar <l3ttyvillamizar@gmail.com>
2023-04-16 17:15:00 -0400

    fix: changes in board

--- src/components/board/components/BoardGame.vue
+++ src/components/board/components/BoardGame.vue
@@ -71,22 +71,19 @@ export default {
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-12 text-center mt-5">
+      <div class="col-12 text-center mt-2">
         <ModalPlayer @get-game-info="getGameInfo" v-if="!player" />
       </div>
     </div>
-    <div class="">
-      Welcome {{player}}
-    </div>
     <div class="row" v-if="player">
-      <div class="col-12">
+      <div class="col-12 mt-2 text-center">
         <h2 class="green">
-          Bienvenido a MemoryGame
+          ¡Hi {{player}}! Welcome to MemoryGame
         </h2>
-        <div>
-          Fallos: {{failures}}
-          Encontradas: {{found}}
-        </div>
+        <h4>
+          <b>Attempts:</b> {{failures}}
+          <b>Hits:</b> {{found}}
+        </h4>
       </div>
       <div class="row">
         <div
@@ -94,7 +91,7 @@ export default {
           v-for="(card, index) in boardCardsRandom"
           :key="index"
         >
-          <img v-if="boardCards[card].show" :src="boardCards[card].image" class="image-box img-thumbnail " alt="...">
+          <img v-if="boardCards[card].show" :src="boardCards[card].image" class="image-box " alt="?">
           <div
             v-if="!boardCards[card].show"
             class=" card-reverse" @click="selectCard(boardCards[card])"
